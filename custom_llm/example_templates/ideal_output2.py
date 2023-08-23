7ae3ae4f41c7af5ea620fadab9b6c2104cb8269e
Kaleb-Nim <65989471+Kaleb-Nim@users.noreply.github.com>
2023-08-23 09:24:22 +0800

    Added .env and LLM files

---
+++ custom_llm/example_templates/ideal_output2.py
@@ -0,0 +1,78 @@
+dict(
+    "list_of_scenes":[
+    dict(
+        "scene": "family trip skiing",
+        "subtitles": [
+          "Are you ready for an unforgettable family ski trip to Japan?",
+          "Ensure your adventure is worry-free with Singlife's travel insurance!"
+        ]
+      ),
+      dict(
+        "scene": "insurance policy document close up",
+        "subtitles": [
+          "Our comprehensive travel insurance plans cater to your needs.",
+          "Peace of mind throughout your journey."
+        ]
+      ),
+      dict(
+        "scene": "beach vacation",
+        "subtitles": [
+          "Escape to paradise with our beach vacation package.",
+          "Experience the sun, sand, and surf like never before!"
+        ]
+      ),
+      dict(
+        "scene": "exploring ancient ruins",
+        "subtitles": [
+          "Embark on a thrilling adventure to discover ancient ruins.",
+          "Uncover the secrets of the past with Singlife as your travel companion."
+        ]
+      ),
+      dict(
+        "scene": "city sightseeing",
+        "subtitles": [
+          "Immerse yourself in the vibrant energy of a bustling city.",
+          "Explore iconic landmarks and create unforgettable memories."
+        ]
+      ),
+      dict(
+        "scene": "wildlife safari",
+        "subtitles": [
+          "Embark on a wildlife safari and witness nature's wonders up close.",
+          "Let Singlife protect you on your extraordinary journey."
+        ]
+      )
+    ]
+),
+ dict(
+                "list_of_scenes":[
+                dict(
+                    "scene": "Scene description1...",
+                    "subtitles": [
+                    "text of video subtitles1...",
+                    "text of video subtitles2..."
+                    ]
+                ),
+                dict(
+                    "scene": "Scene description2...",
+                    "subtitles": [
+                    "text of video subtitles3...",
+                    "text of video subtitles4..."
+                    ]
+                ),
+                dict(
+                    "scene": "Scene description3...",
+                    "subtitles": [
+                    "text of video subtitles5...",
+                    "text of video subtitles6..."
+                    ]
+                ),
+                dict(
+                    "scene": "Scene description4...",
+                    "subtitles": [
+                    "text of video subtitles7...",
+                    "text of video subtitles8..."
+                    ]
+                ),
+                ]
+            )
