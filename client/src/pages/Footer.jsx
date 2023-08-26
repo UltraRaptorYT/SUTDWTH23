6063b25a5629eab1ee6713a3af7e4d989939ca96
Soh Hong Yu <sohhongyu@gmail.com>
2023-08-26 14:24:27 +0800

    Add FontAwesome

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
     <footer className="mt-auto">
       <div className="w-full aspect-[4/1] bg-black">
         <div className="">
-          <i className="fa-solid fa-utensils text-white"></i>
+          <FontAwesomeIcon icon={faCoffee} className="text-white"/>
         </div>
       </div>
     </footer>
