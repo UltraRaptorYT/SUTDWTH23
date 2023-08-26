7c0618be159930b0084595cdeb92b4fc6a18584a
Soh Hong Yu <sohhongyu@gmail.com>
2023-08-26 14:43:41 +0800

    Update Footer.jsx

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faUtensils } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
     <footer className="mt-auto">
-      <div className="w-full aspect-[4/1] bg-black">
-        <div className="">
-          <FontAwesomeIcon icon={faCoffee} className="text-white"/>
+      <div className="w-full h-[70px] pt-[20px]">
+        <div className="bg-red-500 h-full border-t-4 border-black text-xl relative flex items-center justify-center">
+          <div className="flex items-center justify-around gap-12 h-full">
+            <button>
+              <FontAwesomeIcon icon={faUtensils} />
+            </button>
+            <button>
+              <FontAwesomeIcon icon={faUtensils} />
+            </button>
+            <div className="self-start relative w-30">
+              <button className="-translate-y-1/2">
+                <FontAwesomeIcon
+                  type="button"
+                  icon={faUtensils}
+                  className="bg-blue-500 rounded-full aspect-square p-2 border-4 border-black"
+                />
+              </button>
+            </div>
+            <button>
+              <FontAwesomeIcon icon={faUtensils} />
+            </button>
+            <button>
+              <FontAwesomeIcon icon={faUtensils} />
+            </button>
+          </div>
         </div>
       </div>
     </footer>
