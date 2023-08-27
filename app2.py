d898e3c62aab8c578a4e8dc165795f920db4815c
vineesh12344 <nsrvineesh@gmail.com>
2023-08-27 10:59:58 +0800

    not working yet

--- app2.py
+++ app2.py
@@ -302,30 +302,30 @@ async def expire(inputBody: InputModel) -> list:
     return input
 
 
-if __name__ == "__main__":
-	uvicorn.run('app:app', host = '0.0.0.0', port = 8080)
+# if __name__ == "__main__":
+# 	uvicorn.run('app:app', host = '0.0.0.0', port = 8080)
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+# app = FastAPI()
 
-# Set up CORS
-origins = [
-	"*"
-]
+# # Set up CORS
+# origins = [
+# 	"*"
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-@app.post("/RecipeSteps")
-def read_root():
-    return {"Hello": "World"}
+# @app.post("/RecipeSteps")
+# def read_root():
+#     return {"Hello": "World"}
 
 if __name__ == '__main__':
 	# Listening on all network interfaces: 0.0.0.0
